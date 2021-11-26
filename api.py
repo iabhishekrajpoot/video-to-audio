f2f24eade6db701281e48c69131339ccf3d4763c
iabhishekrajpoot <84550714+iabhishekrajpoot@users.noreply.github.com>
2021-11-26 18:02:45 +0530

    Update api.py

--- api.py
+++ api.py
@@ -30,4 +30,4 @@ def main():
         return {"message" : "Something went wrong"},404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=False, host="143.244.137.146", port=5001)
